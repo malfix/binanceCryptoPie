f4618fb7fb50711783accc4d1647a7ef1f05f815
malfix <emanuele.malfara@trovaprezzi.it>
2018-02-27 14:46:47 +0100

    code mini-refactoring

--- view/pie.js
+++ view/pie.js
@@ -1,65 +1,71 @@
-$('body').append("<div id='fixedContainer'></div>");
-$('body').append("<div id='navbar'><span id='show'><img id='icon1' class='icoopen' src=''/></span><span id='hide'><span id='show'><img id='icon2' class='icoclose' src=''/></span></div>");;
+var wallet
+
+$('body').append("<div id='fixedContainer'></div>")
+$('body').append("<div id='navbar'><span id='show'><img id='icon1' class='icoopen' src=''/></span><span id='hide'><span id='show'><img id='icon2' class='icoclose' src=''/></span></div>")
 
 $('#show').click(function() {
-	console.log('show');
-	invert();
-	reload();
-	}
-);
+	invert()
+	reload()
+})
+
 $('#hide').click(function() {
-	console.log('hide');
-	invert();
-	}
-);
+	invert()
+})
 
 $(function() {
 	$('#fixedContainer').hide()
-	$('#hide').hide();
-	$('#show').show();
-	var imgURL = chrome.extension.getURL("img/list.png");
-	document.getElementById("icon1").src = imgURL;
-	document.getElementById("icon2").src = imgURL;
+	$('#hide').hide()
+	$('#show').show()
+	var imgURL = chrome.extension.getURL("img/list.png")
+	document.getElementById("icon1").src = imgURL
+	document.getElementById("icon2").src = imgURL
 	});
 
 function invert(){
-	$('#fixedContainer').toggle();
-	$('#hide').toggle();
-	$('#show').toggle();
+	$('#fixedContainer').toggle()
+	$('#hide').toggle()
+	$('#show').toggle()
 }
 
-function reload() {
-	$('#fixedContainer').text("")	;
-	var wallet = new Wallet();
-
+function initWallet(){
+	if (wallet == undefined) {
+		console.log('init WALLET')
+		wallet = new Wallet()
 		$('div.items.f-cb').each(function (idx, v){
 			if (idx > 0  ) {
 				value = parseFloat($(v).find('.equalValue').text());
-				name = $(v).find('.fullName').text().replace(/(\r\n|\n|\r|\s)/gm,"");
+				name = $(v).find('.fullName').text().replace(/(\r\n|\n|\r|\s)/gm,"")
 				wallet.addCurrency(name, value)
 			}
-		});
-		var datapoints = []
-		for (const [key, value] of wallet.filterBelow(0.01).entries())
-			datapoints.push({y: value, label: key})
-
-		var chart = new CanvasJS.Chart("fixedContainer", {
-			animationEnabled: true,
-			title: {
-				text: "My portfolio"
-			},
-		subtitles:[
-			{
-				text: `Your current balance is about  ${ wallet.balance() } btc`,
-				fontColor: "lightgray"
-			}],
-			data: [{
-				type: "pie",
-				startAngle: 240,
-				yValueFormatString: "##0.0000 \"btc\"",
-				indexLabel: "{label}",
-				dataPoints: datapoints
-			}]
-		});
-		chart.render();
+		})
+	} else {
+		console.log('using existing WALLET')
 	}
+}
+
+function reload() {
+	initWallet()
+	var datapoints = []
+	for (const [key, value] of wallet.filterBelow(0.01).entries())
+		datapoints.push({y: value, label: key})
+
+	var chart = new CanvasJS.Chart("fixedContainer", {
+		animationEnabled: true,
+		title: {
+			text: "My portfolio"
+		},
+	subtitles:[
+		{
+			text: `Your current balance is about  ${ wallet.balance() } btc`,
+			fontColor: "lightgray"
+		}],
+		data: [{
+			type: "pie",
+			startAngle: 240,
+			yValueFormatString: "##0.0000 \"btc\"",
+			indexLabel: "{label}",
+			dataPoints: datapoints
+		}]
+	})
+	chart.render()
+}
